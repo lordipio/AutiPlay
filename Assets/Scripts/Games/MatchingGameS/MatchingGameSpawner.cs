325a3af79da774ddb066cbb90eea559f82a1bfb8
Amirali <88454144+lordipio@users.noreply.github.com>
2025-04-17 04:55:41 +0330

    adding lines

--- Assets/Scripts/Games/MatchingGameS/MatchingGameSpawner.cs
+++ Assets/Scripts/Games/MatchingGameS/MatchingGameSpawner.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
 using System.IO;
 using System.Security.Cryptography;
 using UnityEngine;
@@ -11,14 +12,107 @@ public class MatchingGameSpawner : MonoBehaviour
     List<MatchingIcon> topMatchingIcon = new List<MatchingIcon>();
     List<MatchingIcon> buttomMatchingIcon = new List<MatchingIcon>();
 
+    List<LineRenderer> lines = new List<LineRenderer>();
+
+    int selectedIconIndex = -1;
+
+    LineRenderer line;
+
+    enum SelectedIconSide
+    {
+        top,
+        bottom,
+        none
+    }
+
+    SelectedIconSide selectedIconSide = SelectedIconSide.none;
 
     void Start()
     {
+
+        line = gameObject.AddComponent<LineRenderer>();
+        line.positionCount = 2;
+        line.startWidth = 0.5f;
+        line.endWidth = 0.5f;
+        line.enabled = false;
+
+        SpawnIcons();
+
+
+      
+
+
+    }
+
+
+
+    void Update()
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+            line.enabled = false;
+
+            selectedIconIndex = -1;
+
+            selectedIconSide = SelectedIconSide.none;
+        }
+
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        line.SetPosition(1, mousePos);
+
+//#if UNITY_EDITOR
+//        // استفاده از موس توی ادیتور
+//        if (Input.GetMouseButtonDown(0))
+//                {
+//                    Vector2 worldPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+//                    Collider2D hit = Physics2D.OverlapPoint(worldPoint);
+
+//                    if (hit != null && hit == GetComponent<Collider2D>())
+//                    {
+//                        Debug.Log("Clicked on the circle!");
+//                    }
+//                }
+//        #else
+//                // استفاده از تاچ توی موبایل
+//                if (Input.touchCount > 0)
+//                {
+//                    Touch touch = Input.GetTouch(0);
+
+//                    if (touch.phase == TouchPhase.Began)
+//                    {
+//                        Vector2 worldPoint = Camera.main.ScreenToWorldPoint(touch.position);
+//                        Collider2D hit = Physics2D.OverlapPoint(worldPoint);
+
+//                        if (hit != null && hit == GetComponent<Collider2D>())
+//                        {
+//                            Debug.Log("Touched the circle!");
+//                        }
+//                    }
+//                }
+//        #endif
+    }
+
+    void SpawnIcons()
+    {
+        selectedIconIndex = -1;
+
+        topMatchingIcon = new List<MatchingIcon>();
+
+        buttomMatchingIcon = new List<MatchingIcon>();
+
+        lines = new List<LineRenderer>();
+
+
+
         List<Vector2> Positions = SpawnUtility.GetUpDividedPositions(4);
-        
+
         GameObject tempMatchingIconGO;
 
         MatchingIcon tempMatchingIcon;
+
+        List<MatchingIcon> matchingIcons = new List<MatchingIcon>();
+
 
         Texture2D[] allTextures = SpawnUtility.LoadTextures(SpawnUtility.IconTypes.Animals);
 
@@ -26,7 +120,8 @@ public class MatchingGameSpawner : MonoBehaviour
 
         List<Texture2D> textures = new List<Texture2D>();
 
-        foreach(int i in texturesRandomSelection)
+
+        foreach (int i in texturesRandomSelection)
         {
             textures.Add(allTextures[i]);
         }
@@ -44,11 +139,15 @@ public class MatchingGameSpawner : MonoBehaviour
             tempMatchingIcon.spriteRenderer.sprite = Sprite.Create(
             tex,
             new Rect(0, 0, tex.width, tex.height),
-            new Vector2(0.5f, 0.5f) );
+            new Vector2(0.5f, 0.5f));
             tempMatchingIcon.SetKnobToTheTop();
             tempMatchingIcon.iconIndex = temp;
             topMatchingIcon.Add(tempMatchingIcon);
+
+            tempMatchingIcon.onIconCollided += OnTopIconCollided;
+
             temp++;
+
         }
 
         Positions = SpawnUtility.GetDownDividedPositions(4);
@@ -63,19 +162,122 @@ public class MatchingGameSpawner : MonoBehaviour
 
             tempMatchingIcon = tempMatchingIconGO.GetComponent<MatchingIcon>();
 
-            Texture2D tex = textures[texturesRandomSelection[temp]];
 
             tempMatchingIcon.SetKnobToTheButtom();
 
-            tempMatchingIcon.iconIndex = temp; // change it
 
-            tempMatchingIcon.spriteRenderer.sprite = Sprite.Create(
-            tex,
-            new Rect(0, 0, tex.width, tex.height),
-            new Vector2(0.5f, 0.5f));
+            tempMatchingIcon.spriteRenderer.sprite = topMatchingIcon[texturesRandomSelection[temp]].spriteRenderer.sprite;
+
+            tempMatchingIcon.iconIndex = topMatchingIcon[texturesRandomSelection[temp]].iconIndex;
 
             buttomMatchingIcon.Add(tempMatchingIcon);
+
+            tempMatchingIcon.onIconCollided += OnButtomIconCollided;
+
             temp++;
         }
+
+    }
+
+
+    void OnTopIconCollided(int iconIndex)
+    {
+        if (iconIndex == selectedIconIndex && selectedIconSide == SelectedIconSide.bottom)
+        {
+            MatchingIcon collidedMatchingIcon = null;
+
+            foreach (MatchingIcon matchingIcon in buttomMatchingIcon)
+                if (matchingIcon.iconIndex == iconIndex)
+                    collidedMatchingIcon = matchingIcon;
+
+            CreateNewLine(topMatchingIcon[iconIndex].holderKnob.transform.position, collidedMatchingIcon.holderKnob.transform.position);
+
+            print("SEX");
+
+        }
+
+
+        selectedIconIndex = iconIndex;
+        selectedIconSide = SelectedIconSide.top;
+
+        line.enabled = true;
+
+        line.SetPosition(0, topMatchingIcon[iconIndex].holderKnob.transform.position);
+
+        print(iconIndex);
+    }
+
+    void OnButtomIconCollided(int iconIndex)
+    {
+        MatchingIcon collidedMatchingIcon = null;
+
+        foreach (MatchingIcon matchingIcon in buttomMatchingIcon)
+            if (matchingIcon.iconIndex == iconIndex)
+                collidedMatchingIcon = matchingIcon;
+
+        if (iconIndex == selectedIconIndex && selectedIconSide == SelectedIconSide.top)
+        {
+            CreateNewLine(topMatchingIcon[selectedIconIndex].holderKnob.transform.position, collidedMatchingIcon.holderKnob.transform.position);
+            print("SEX");
+            StartCoroutine(RemoveObjects());
+        }
+
+        selectedIconIndex = iconIndex;
+        selectedIconSide = SelectedIconSide.bottom;
+
+        line.enabled = true;
+
+        line.SetPosition(0, collidedMatchingIcon.holderKnob.transform.position);
+
+        print(iconIndex);
+    }
+
+
+    void CreateNewLine(Vector2 startPos, Vector2 endPos)
+    {
+        GameObject lineObj = new GameObject("Line");
+        LineRenderer lr = lineObj.AddComponent<LineRenderer>();
+        lr.positionCount = 2;
+        lr.startWidth = 0.2f;
+        lr.endWidth = 0.2f;
+        lr.SetPosition(0, startPos);
+        lr.SetPosition(1, endPos);
+        lr.enabled = true;
+        lines.Add(lr);
+    }
+
+
+    IEnumerator RemoveObjects()
+    {
+        yield return new WaitForSeconds(2f);
+        
+        if (topMatchingIcon.Count > 0)
+            foreach(MatchingIcon matchingIcon in topMatchingIcon)
+            {
+                GameObject.Destroy(matchingIcon.gameObject);
+            }
+
+        if (buttomMatchingIcon.Count > 0)
+            foreach (MatchingIcon matchingIcon in buttomMatchingIcon)
+            {
+                GameObject.Destroy(matchingIcon.gameObject);
+            }
+
+        if (lines.Count > 0)
+            foreach (LineRenderer lineRenderer in lines)
+            {
+                GameObject.Destroy(lineRenderer.gameObject);
+            }
+
+        topMatchingIcon = null;
+
+        buttomMatchingIcon = null;
+
+        lines = null;
+
+
+        yield return new WaitForSeconds(1f);
+        SpawnIcons();
     }
 }
+
--- Assets/Scripts/Games/MatchingGameS/MatchingIcon.cs
+++ Assets/Scripts/Games/MatchingGameS/MatchingIcon.cs
@@ -1,14 +1,21 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Rendering.Universal;
 
 public class MatchingIcon : MonoBehaviour
 {
     public SpriteRenderer spriteRenderer;
     public GameObject holderKnob;
+    private CircleCollider2D circleCollider;
     [HideInInspector] public int iconIndex = -1;
+
+    public delegate void OnIconCollided(int iconIndex);
+    public OnIconCollided onIconCollided;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -26,4 +33,20 @@ public class MatchingIcon : MonoBehaviour
     {
         holderKnob.transform.localPosition = new Vector3(0, 3.5f, 0);
     }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+    }
+
+    public void OnMouseDown() // touch
+    {
+        onIconCollided?.Invoke(iconIndex);
+    }
+
+    public void OnMouseEnter()
+    {
+        if (Input.GetMouseButton(0))
+           onIconCollided?.Invoke(iconIndex);
+    }
+
 }
